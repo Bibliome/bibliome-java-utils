42283aa13ccc4382c6c70d37c23f0c031f319e0b
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2017-09-20 09:26:57 +0200

    generate command-line tools for pubmed index

--- install.sh
+++ install.sh
@@ -25,3 +25,5 @@ fi
 ./make-java-launcher.sh "$LIB_DIR" "$BIN_DIR"/tbrep                  org.bibliome.util.pattern.tabular.TabularPattern
 ./make-java-launcher.sh "$LIB_DIR" "$BIN_DIR"/sample                 org.bibliome.util.Sampler
 ./make-java-launcher.sh "$LIB_DIR" "$BIN_DIR"/select-pubmed          org.bibliome.util.pubmed.PubMedSelect
+./make-java-launcher.sh "$LIB_DIR" "$BIN_DIR"/pubmed-index           org.bibliome.util.pubmed.PubMedIndexUpdater
+./make-java-launcher.sh "$LIB_DIR" "$BIN_DIR"/pubmed-search          org.bibliome.util.pubmed.PubMedIndexSearcher
